1b04c46fef19270d84579a4bed99751fe281d6b6
Dato Tsertsvadze <157514264+dato2003@users.noreply.github.com>
2024-03-25 22:07:24 +0400

    Add files via upload

---
+++ User/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+
+    def age(self):
+        import datetime
+        today = datetime.date.today()
+        return today.year - self.birth_date.year - ((today.month, today.day) < (self.birth_date.month, self.birth_date.day))
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
