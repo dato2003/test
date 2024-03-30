753f449e060a11fb8d57c52dafb86beb4ffa080a
Dato Tsertsvadze <157514264+dato2003@users.noreply.github.com>
2024-03-30 22:48:31 +0400

    Delete User directory

--- User/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from .models import UserProfile
-
-def user_info(request):
-    user_profile = UserProfile.objects.first() 
-    return HttpResponse(f"Name: {user_profile.first_name} {user_profile.last_name}, Age: {user_profile.age()}")
-
